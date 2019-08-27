e567069a3e30405ef50b40f03280ba8e251a51f8
lixiaotao <lixiaotao@hotmail.com>
2019-08-27 15:48:08 +1200

    redux debug tools

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ export default function configureStore(initialState = {}) {
   return createStore(
     rootReducer,
     //    initialState,
- 
+ // make debug tools redux
     composeEnhancers(applyMiddleware(thunk))
   );
 }
